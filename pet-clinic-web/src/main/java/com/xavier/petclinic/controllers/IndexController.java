e25fe75d0425844f8aa325770bf3c4daea012556
captainxavier <osisxavier@gmail.com>
2020-10-08 19:37:14 +0300

    adding master vets layout. Closes #18

--- pet-clinic-web/src/main/java/com/xavier/petclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/com/xavier/petclinic/controllers/IndexController.java
@@ -2,24 +2,13 @@ package com.xavier.petclinic.controllers;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 public class IndexController {
 
-    @RequestMapping({"","/","index","index.html"})
-    public String index(Model model){
-        Map<String,String> test1 = new HashMap<String, String>();
-        test1.put("1","2");
-        test1.put("3","4");
-        test1.put("5","6");
-        test1.put("7","8");
-        model.addAttribute("tests",test1);
-        String ans = "Testinf";
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index(){
         return "index";
     }
 }
